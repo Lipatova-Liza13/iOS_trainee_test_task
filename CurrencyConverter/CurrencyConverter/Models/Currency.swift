e45199d9e227813cba6e4ff915c7e420a4a8f9a2
Liza Lipatova <lizalipatova13@gmail.com>
2021-01-14 13:32:26 +0200

    create Currency model

---
+++ CurrencyConverter/CurrencyConverter/Models/Currency.swift
@@ -0,0 +1,13 @@
+//
+//  Currency.swift
+//  CurrencyConverter
+//
+//  Created by Macbook on 14.01.2021.
+//
+
+import Foundation
+
+struct Currency {
+    let currencyName: String
+    let rate: String
+}
