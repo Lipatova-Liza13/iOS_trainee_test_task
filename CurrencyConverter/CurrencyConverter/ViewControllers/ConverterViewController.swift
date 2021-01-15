34ae3f237357a50c94d01e5575dca2ace08e4d0d
Liza Lipatova <lizalipatova13@gmail.com>
2021-01-15 03:27:50 +0200

    add ConverterViewController

---
+++ CurrencyConverter/CurrencyConverter/ViewControllers/ConverterViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ConverterViewController.swift
+//  CurrencyConverter
+//
+//  Created by Macbook on 14.01.2021.
+//
+
+import UIKit
+
+class ConverterViewController: UIViewController {
+
+    @IBOutlet weak var startCurrencyPickerView: UIPickerView!
+    @IBOutlet weak var endCurrencyPickerView: UIPickerView!
+    @IBOutlet weak var startCurrencyLabel: UILabel!
+    @IBOutlet weak var endCurrencyLabel: UILabel!
+    @IBOutlet weak var startAmountTextField: UITextField!
+    @IBOutlet weak var changedAmountTextField: UILabel!
+    var currencies = [Currency]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadFiles()
+        self.startCurrencyPickerView.delegate = self
+        self.startCurrencyPickerView.dataSource = self
+        self.endCurrencyPickerView.delegate = self
+        self.endCurrencyPickerView.dataSource = self
+    }
+    
+    private func loadFiles() -> Void {
+        Provider().fetchCurrencyJSON { (res) in
+            switch res {
+            case .success(let currencyResult):
+                self.currencies.append(contentsOf: currencyResult)
+                DispatchQueue.main.async {
+                    self.startCurrencyPickerView.reloadAllComponents()
+                    self.endCurrencyPickerView.reloadAllComponents()
+                }
+            case .failure(let error):
+                print("The error is: \(error)")
+            }
+        }
+    }
+}
+
+extension ConverterViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencies.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencies[row].currencyName
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView == startCurrencyPickerView
+        {
+            startCurrencyLabel.text = currencies[row].currencyName
+        }
+        else
+        {
+            endCurrencyLabel.text = currencies[row].currencyName
+        }
+    }
+}
